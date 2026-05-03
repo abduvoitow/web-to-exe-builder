fc273f75a5f3e1012ea9d268f640959f18919be2
abduvoitow <abduvoitov@gmail.com>
2026-05-03 23:42:19 +0500

    Fix socket double initialization bug

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ APP_NAME = "WebToEXE App"
 
 # Port acquisition
 def get_free_port():
-    s = socket.socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 0))
     port = s.getsockname()[1]
     s.close()
